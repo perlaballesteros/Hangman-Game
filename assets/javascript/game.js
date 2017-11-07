5a6cdcd8b5daae77e2865b2e53c5034cc9280741
rubib <rubib@email.arizona.edu>
2017-11-07 15:39:42 -0700

    the javas script produces a hangman game, will now be moving on to structure and styling of the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,15 @@
 
 //-----------GLOBALVARIABLES---------------------------
 //Human Anatomy themed words
-var hangmanWords=["disco","brain","groovie","phalanges"];
+var hangmanWords=["brain","brain","groovie","phalanges"];
 //HOLD THE STRING OF THE WORD IN THE HANGMANWORDS
 var word2Guess;
 //WILL WORD2GUESS AS AN ARRAY TYPE
 var word2Array;
 //AN ARRAY WITH "_"
 var blankArray;
+//CHECKS IF GUESS MATCHES THE STIRNG IN INDEX.
+var match= false;	
 //NUMBER OF GUESSES ALLOWED
 var guessesAllowed=7;
 var guessesRemaining=7;
@@ -21,13 +23,12 @@ var lettersGuessed=0;
 var wins=0;
 //STORES THE USERS DECISION TO CONTINUE OR END THE GAME
 var continueGame;
-//this will be the counter to move through the words in the hangman array
+//COUNTER VARIABLE TO MOVE THEROUGH WORDS IN HANGMAN ARRAY
 var nextWord=0;
-//EXITING THE WHILE LOOP
-var endGame=false;
-
-
-
+//MAKING SURE THAT USER WORD HAS NOT BEEN GUESSED ALREADY
+var wordAlreadyguessed=false;
+//STORES BOOLEAN TO CHECK IF USER'S GUESS IS REPEATED
+var userWrongguessRepeated;
 
 
 
@@ -71,10 +72,33 @@ function insertBlanksguesses()
 newWord2guess(nextWord);
 
 
-//CHECKING
-console.log("word2arrayb4 onkey: "+ word2Array);
-console.log("blankArray b4 "+ blankArray);
-console.log("guessesWrong b4: " +guessesWrong);
+function checkingUserguess()
+{
+	//CHECKS REPEAT USER GUESS IN WORD TO GUESS
+	for(var i=0;i<word2Array.length;i++)
+	{
+		
+		if(blankArray[i]===userGuess)
+		{
+			wordAlreadyguessed=true;
+		}
+		
+	}
+	//CHECKS REPEATS IN WRONG GUESSES
+	for(var j=0;j<guessesWrong.length;j++)
+	{
+		
+		if(guessesWrong[j]===userGuess)
+		{
+
+			userWrongguessRepeated=true;
+		
+		}
+	}
+}
+
+
+
 //------------------------------------------ONKEYUP FUNCTION-----------------------------------------------
 
 
@@ -84,99 +108,131 @@ document.onkeyup = function(event)
 
 	//USER GUESS
 	userGuess= event.key;
-	console.log(userGuess);	
-	//CHECKS IF GUESS MATCHES THE STIRNG IN INDEX.
-	var match= false;	
-	//you win 
-	var youWin=word2Array.length
-//CHECK
-console.log("youWin" + youWin);
+	console.log(userGuess);
+
+	//BEFORE FUNCTION SO PREVIOUS GUESS DOEN'T AFFECT NEW(IMPORTANT)
+	userWrongguessRepeated=false;	
+	match=false;
 
 
+	//GET ARRAYS THAT HOLD THE "_"
+	checkingUserguess();
+	
+
+	if(wordAlreadyguessed===false)
+	{
+	
+		//WIN IF YOU WIN IS EQUAL TO THE LENGTH OF THE ARRAY WITH THE WORD THAT IS BEING GUESSED
+		var youWin=word2Array.length
 
 
+		
+		for(var i=0;i<word2Array.length;i++)
+		{
+	
+		
+			//CHEKING IF USER GUESS IS A LETTER IN THE WORD
+			if(word2Array[i]===userGuess)
+			{
+				blankArray[i]=userGuess;
+				lettersGuessed++;
+				match=true;
 
+			}
+			
+		}
 
-	//TO DO:add a check for space and non letter before start
-	for(var i=0;i<word2Array.length;i++)
+		
+	
+		//IF WRONG GUESS SUBTRACT FROM THE GUESSES REMAINDING
+		//THIS ONE BELOW SHOULD RUN THE FIRST TIME THE GAME IS PLAYED AND WHEN NO REPEATED WORDS
+		if(match===false && userWrongguessRepeated===false)
+		{
+			//REPLACING ARRAY BLANK WITH USER GUESS
+
+			guessesWrong[x]=userGuess;
+			x++;
+			guessesRemaining--;
+			
+			
+		}
+
+		
+		console.log("---------------------------");
+		console.log("lettersGuessed" + lettersGuessed);
+		console.log("youWin" + youWin);
+		console.log("--------------------------------");
+
+		//IF USER WINS
+		if (lettersGuessed===youWin)
+		{
+
+			continueGame=confirm("Congrats, you have won! Would you like to guess another word?");
+			wins++;
+
+			if(continueGam=true)
+			{
+				nextWord++;
+				newWord2guess(nextWord);
+				lettersGuessed=0;
+				guessesRemaining=7;
+			}
+	
+		
+		}
+
+		//IF PLAYER RUNS OUT OF guesses
+		if (guessesRemaining===0)
+		{
+
+			continueGame=confirm("You have run out of guesses,better luck next time! Would you like to guess another word?");
+
+			if(continueGame===true)
+			{
+				//ADDING ONE IN ORDER TO ACCESS THE NEXT WORD
+				nextWord++;
+				//CALING THE FUNCTIONS TO RECEIVE THE NEXT WORD
+				newWord2guess(nextWord);
+				//REINITIALIZING THE LETTERS GUESS TO ZERO FOR THE NEW WORD
+				lettersGuessed=0;
+				//REINITIALIZING THE # OF TRIES TO 7 FOR THE NEW WORD
+				guessesRemaining=7;
+				//REINITIALIZING IN ORDER TO GO BACK TO THE START OF THE GUESSEDWORDS ARRAY FOR THE NEW WORD
+				x=0;
+			}
+
+				
+		}
+	console.log("---------------------------");
+	console.log("Word to guess "+ word2Array);
+	console.log("blankArray "+ blankArray);
+	console.log("wrong guesses " + guessesWrong);
+	console.log("guesses remaining "+ guessesRemaining);
+	console.log("---------------------------");	
+		
+	}
+					
+	if(wordAlreadyguessed===true)
 	{
 		
-		//CHEKING IF USER GUESS IS A LETTER IN THE WORD
-		if(word2Array[i]===userGuess)
-		{
-			blankArray[i]=userGuess;
-			lettersGuessed++;
-//CHECKING
-console.log("letter" + lettersGuessed);
-			match=true;
+			alert("you have already correctly guessed letter, " + userGuess + "." + "press a different letter to continue.");
+			//REINITIALIZING TO BE ABLE TO CHECK IF THE NEW LETTER HAS REPEATED
+			wordAlreadyguessed=false;
 
-		}
-
-	}
-
-
-//CHEKING
-console.log("blankArray after for loop check: " + blankArray);
-
-
-	//IF WRONG GUESS SUBTRACT FROM THE GUESSES REMAINDING
-	if(match===false)
-	{
-		//REPLACING ARRAY BLANK WITH USER GUESS
-		guessesWrong[x]=userGuess;
-		guessesRemaining--;
-		x++;	
-//CHECKING
-console.log("guessesWrong after wrong letter: " + guessesWrong);
-console.log("gyessesReainingafter first mistake "+ guessesRemaining);
-	}
-
-
-	//IF USER WINS
-	if (lettersGuessed===youWin)
-	{
-
-		continueGame=confirm("Congrats, you have won! Would you like to guess another word?");
-		wins++;
-
-		if(continueGam=true)
-		{
-			nextWord++;
-			newWord2guess(nextWord);
-			lettersGuessed=0;
-			guessesRemaining=7;
-		}
-		else{
-			endGame=true;
-		}
-		
-	}
-	//IF PLAYER RUNS OUT OF guesses
-	if (guessesRemaining===0)
-	{
-		continueGame=confirm("You have run out of guesses,better luck next time! Would you like to guess another word?");
-
-		if(continueGam=true)
-		{
-			nextWord++;
-			newWord2guess(nextWord);
-			lettersGuessed=0;
-			guessesRemaining=7;
-			x=0;
-		}
-
-		else{
-			endGame=true;
-		}
 	}
 	
-}
+	
+}//onkey function
 
-
-
-
-
-//-----------------------------------------------------------
-
+//----------------------
+//if(match===false && userWrongguessRepeated===true)
+		//{
+			////REPLACING ARRAY BLANK WITH USER GUESS
+			//guessesWrong[x]=userGuess;
+			//x++;
+			//console.log(2);
+			
+			
+		//}
 
 
